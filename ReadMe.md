7314c3949c29419b582ba955d1cae894a86282f0
Varun Agarwal <varunagarwal315@gmail.com>
2019-08-28 12:39:59 +0530

    Cleaned some files

---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+### Learning Project for AX
+
+#### Important resources
+- To clean code use - `https://beautifier.io/`
+- Ensure all changes are on a `*-dev` branch
+- If it all works, merge to master
+- Don't push more than 100-150 changes at a time
--- activity.php
+++ activity.php
@@ -1,84 +1,83 @@
-
-  <div class="container-fluid activitycontainer">
-   <div class="row arow">
-    <div class="col-md-6 offset-md-4">
-	
-	   <br>
-	   <div class="btn-group" role="group">
-	     <a  href="#home"><button onclick="myFunction()" class="btn btn-dark" id="acbutton">SIGHT SEEING</button></a>
-		 <a href="#act"><button onclick="myFunction2()" class="btn btn-dark" id="acbutton">ACTIVITIES TO DO</button></a>
-		 <a href="#contact"><button onclick="myFunction3()" class="btn btn-dark" id="acbutton">SHOPPING</button></a>
-	    </div>
-      </div>
-  </div>
-	 <br>
-	                                  <!-- Sight seeing-->
-	 <div id="myDIV">
-    <div class="row arow">
-	   <div class="col-md-4">
-	     <img src="images/travel1.jpg" alt="Dudhsagar Waterfall" class="img-fluid img-thumbnail" width="70%">
-		  <h1>Dudhsagar Falls</h1>
-		  <p title=" Dudhsagar Waterfall" class="dis">The magnificent Dudhsagar Waterfall is perched in the high peaks
-		  of the Western Ghats and is a sight to behold especially in the monsoons when it is in full and furious flow.</p>
-		  <a title=" Dudhsagar Waterfall" href="https://en.wikipedia.org/wiki/Dudhsagar_Falls" class="an">Read more</a>
-		</div>
-		 <div class="col-md-4">
-		    <img src="images/travel2.jpg" alt="Aguada" class="img-fluid img-thumbnail" width="70%" >
-		    <h1>Fort Aguada</h1>
-		    <p title="Aguada" class="dis">Aguada is a Portuguese word which means "Watering Place" and the fort was built by the then Portuguese rulers for providing fresh water supply to the ships passing by in the Arabian Sea.</p>
-		    <a title="Aguada" href="https://en.wikipedia.org/wiki/Fort_Aguada" class="an">Read more</a>
-	     </div>
-		 <div class="col-md-4">
-		    <img src="images/travel3.jpg" alt="Basilica of Bom Jesus" class="img-fluid img-thumbnail"  width="70%">
-		    <h1>Basilica of Bom Jesus</h1>
-		    <p title="Basilica of Bom Jesus" class="dis">The Basilica of Bom Jesus or Borea Jezuchi Bajilika is located in Goa, India. The basilica holds the mortal remains of St. Francis Xavier. </p>
-		    <a title="Basilica of Bom Jesus" href="https://en.wikipedia.org/wiki/Basilica_of_Bom_Jesus" class="an">Read more</a>
-		</div>
-
-  </div>
-  </div>
-                                 <!-- Activities to do-->
- <div id="myDIV2">
+<div class="container-fluid activitycontainer">
   <div class="row arow">
-     <div class="col-md-4">
-        <img src="images/activity1.jpg" alt="water sports" class="img-fluid img-thumbnail" width="70%">
-		  <h1>Adventure water sports</h1>
-		  <p title="Adventure water sports">A trip to Goa can never be complete with enjoying water sports!One of the best hubs for water sports in the country,the beaches and the waterfronts of Goa make it<br> for an international water sports destination.</p>
-          <a title="Adventure water sports" href="https://www.thrillophilia.com/tours/adventurous-water-sports-in-goa?" class="an">Book now</a>
+    <div class="col-md-6 offset-md-4">
+
+      <br>
+      <div class="btn-group" role="group">
+        <a href="#home"><button onclick="myFunction()" class="btn btn-dark" id="acbutton">SIGHT SEEING</button></a>
+        <a href="#act"><button onclick="myFunction2()" class="btn btn-dark" id="acbutton">ACTIVITIES TO DO</button></a>
+        <a href="#contact"><button onclick="myFunction3()" class="btn btn-dark" id="acbutton">SHOPPING</button></a>
       </div>
-	   <div class="col-md-4">
-	      <img src="images/activity2.jpg" alt="Trekking" class="img-fluid img-thumbnail" width="70%">
-		  <h1>Trekking</h1>
-		  <p title="Trekking">Planning a Goa trip with only the beaches and party in mind is a grave mistake as it offers some</p>
-		  <a title="Trekking" href="https://www.thrillophilia.com/tours/overnight-trek-to-tambdi-surla" class="an">Book now</a>
-	   </div>
-	   <div class="col-md-4">
-	      <img src="images/activity3.jpg" alt="Hiking" class="img-fluid img-thumbnail" width="70%">
-		  <h1>Hiking</h1>
-		  <p title="Hiking">An outdoor activity, hiking usually involves walking on paths or
-		  tracks that are pretty much challenging and offers an offbeat experience of exploring the least visited vicinities</p>
-		  <a title="Hiking" href="https://www.thrillophilia.com/tours/jungle-adventure-in-goa" class="an">Book now</a>
-		</div>
     </div>
- </div>
-		                              <!-- Shopping-->
-		<div id="myDIV3">
-		 <div class="row arow">
-           <div class="col-md-4">
-		     <img src="images/market1.jpeg" alt="Anjuna Flea Market" class="img-fluid img-thumbnail" width="70%">
-		     <h1>Anjuna Flea Market</h1>
-		     <p title="Anjuna Flea Market">The liveliest of all markets in Goa,it is held every Wednesday in the beach town of Anjuna in North<br> Goa. </p>
-			</div>
-			<div class="col-md-4">
-			   <img src="images/market2.jpg"  alt="Calangute Market Square" class="img-fluid img-thumbnail" width="70%">
-		       <h1>Calangute Market Square</h1>
-		       <p title="Calangute Market Square">The popular Calangute beach in Goa is lined with innumerable shacks and stalls making it one of the top markets of this shopping paradise.</p>
-			 </div>
-			 <div class="col-md-4">
-			    <img src="images/market3.jpg" alt="Saturday Night Bazaar at Arpora" class="img-fluid img-thumbnail" width="70%" >
-		        <h1>Saturday Night Bazaar at Arpora</h1>
-		        <p title="Saturday Night Bazaar at Arpora">There is no better way to spend a Saturday evening in Goathan by visiting the Night Bazaar at Arpora.</p>
-			 </div>
-		</div>
-	</div>
   </div>
+  <br>
+  <!-- Sight seeing-->
+  <div id="myDIV">
+    <div class="row arow">
+      <div class="col-md-4">
+        <img src="images/travel1.jpg" alt="Dudhsagar Waterfall" class="img-fluid img-thumbnail" width="70%">
+        <h1>Dudhsagar Falls</h1>
+        <p title=" Dudhsagar Waterfall" class="dis">The magnificent Dudhsagar Waterfall is perched in the high peaks
+          of the Western Ghats and is a sight to behold especially in the monsoons when it is in full and furious flow.</p>
+        <a title=" Dudhsagar Waterfall" href="https://en.wikipedia.org/wiki/Dudhsagar_Falls" class="an">Read more</a>
+      </div>
+      <div class="col-md-4">
+        <img src="images/travel2.jpg" alt="Aguada" class="img-fluid img-thumbnail" width="70%">
+        <h1>Fort Aguada</h1>
+        <p title="Aguada" class="dis">Aguada is a Portuguese word which means "Watering Place" and the fort was built by the then Portuguese rulers for providing fresh water supply to the ships passing by in the Arabian Sea.</p>
+        <a title="Aguada" href="https://en.wikipedia.org/wiki/Fort_Aguada" class="an">Read more</a>
+      </div>
+      <div class="col-md-4">
+        <img src="images/travel3.jpg" alt="Basilica of Bom Jesus" class="img-fluid img-thumbnail" width="70%">
+        <h1>Basilica of Bom Jesus</h1>
+        <p title="Basilica of Bom Jesus" class="dis">The Basilica of Bom Jesus or Borea Jezuchi Bajilika is located in Goa, India. The basilica holds the mortal remains of St. Francis Xavier. </p>
+        <a title="Basilica of Bom Jesus" href="https://en.wikipedia.org/wiki/Basilica_of_Bom_Jesus" class="an">Read more</a>
+      </div>
+
+    </div>
+  </div>
+  <!-- Activities to do-->
+  <div id="myDIV2">
+    <div class="row arow">
+      <div class="col-md-4">
+        <img src="images/activity1.jpg" alt="water sports" class="img-fluid img-thumbnail" width="70%">
+        <h1>Adventure water sports</h1>
+        <p title="Adventure water sports">A trip to Goa can never be complete with enjoying water sports!One of the best hubs for water sports in the country,the beaches and the waterfronts of Goa make it<br> for an international water sports destination.</p>
+        <a title="Adventure water sports" href="https://www.thrillophilia.com/tours/adventurous-water-sports-in-goa?" class="an">Book now</a>
+      </div>
+      <div class="col-md-4">
+        <img src="images/activity2.jpg" alt="Trekking" class="img-fluid img-thumbnail" width="70%">
+        <h1>Trekking</h1>
+        <p title="Trekking">Planning a Goa trip with only the beaches and party in mind is a grave mistake as it offers some</p>
+        <a title="Trekking" href="https://www.thrillophilia.com/tours/overnight-trek-to-tambdi-surla" class="an">Book now</a>
+      </div>
+      <div class="col-md-4">
+        <img src="images/activity3.jpg" alt="Hiking" class="img-fluid img-thumbnail" width="70%">
+        <h1>Hiking</h1>
+        <p title="Hiking">An outdoor activity, hiking usually involves walking on paths or
+          tracks that are pretty much challenging and offers an offbeat experience of exploring the least visited vicinities</p>
+        <a title="Hiking" href="https://www.thrillophilia.com/tours/jungle-adventure-in-goa" class="an">Book now</a>
+      </div>
+    </div>
+  </div>
+  <!-- Shopping-->
+  <div id="myDIV3">
+    <div class="row arow">
+      <div class="col-md-4">
+        <img src="images/market1.jpeg" alt="Anjuna Flea Market" class="img-fluid img-thumbnail" width="70%">
+        <h1>Anjuna Flea Market</h1>
+        <p title="Anjuna Flea Market">The liveliest of all markets in Goa,it is held every Wednesday in the beach town of Anjuna in North<br> Goa. </p>
+      </div>
+      <div class="col-md-4">
+        <img src="images/market2.jpg" alt="Calangute Market Square" class="img-fluid img-thumbnail" width="70%">
+        <h1>Calangute Market Square</h1>
+        <p title="Calangute Market Square">The popular Calangute beach in Goa is lined with innumerable shacks and stalls making it one of the top markets of this shopping paradise.</p>
+      </div>
+      <div class="col-md-4">
+        <img src="images/market3.jpg" alt="Saturday Night Bazaar at Arpora" class="img-fluid img-thumbnail" width="70%">
+        <h1>Saturday Night Bazaar at Arpora</h1>
+        <p title="Saturday Night Bazaar at Arpora">There is no better way to spend a Saturday evening in Goathan by visiting the Night Bazaar at Arpora.</p>
+      </div>
+    </div>
+  </div>
+</div>
