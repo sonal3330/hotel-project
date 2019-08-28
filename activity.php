2e93551ac39cd18e7ed7b6558203981db540ea74
Varun Agarwal <varunagarwal315@gmail.com>
2019-08-28 12:17:06 +0530

    Cleaned files on skype

--- activity.php
+++ activity.php
@@ -1,72 +1,4 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-     <meta name="description" content="Rajmahal hotel"> 
-    <meta name="keywords" content="Rajmahal hotel, hotel in Goa, best hotel"> 
-    <meta name="author" content="Sonal Sharma"> 
-	    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<link rel="stylesheet" href="css/activitycss.css"> 
-	 <script>
-	
-      function myFunction() {
 
-	    var y = document.getElementById("myDIV2");
-        var x = document.getElementById("myDIV");
-	    var z = document.getElementById("myDIV3");
-        if (x.style.display === "none") {
-          x.style.display = "block";
-		  y.style.display = "none";
-		  z.style.display = "none";
-        } 
-		else {
-          x.style.display = "block";
-	      y.style.display = "none";
-	      z.style.display = "none";
-        }
-	  }
-	  
-      function myFunction2() {
-	
-	    var y = document.getElementById("myDIV2");
-        var x = document.getElementById("myDIV");
-	    var z = document.getElementById("myDIV3");
-        if (y.style.display === "none") {
-		  z.style.display = "none";
-		  x.style.display = "none";
-          y.style.display = "block";
-		} 
-		else {
-          x.style.display = "none";
-	      z.style.display = "none";
-	      y.style.display = "block";
-        }
-      }
-	  
-      function myFunction3() {
-	
-        var x = document.getElementById("myDIV");
-	    var y = document.getElementById("myDIV2");
-	    var z = document.getElementById("myDIV3");
-        if (z.style.display === "none") {
-		  x.style.display = "none";
-		  y.style.display = "none";
-          z.style.display = "block";
-		}
-		else {
-          x.style.display = "none";
-	      x.style.display = "none";
-		  z.style.display =  "block";
-        }
-      }
-	  
-    </script>
-	<noscript>Sorry, your browser does not support JavaScript!</noscript>
-  </head>
-  <body>
   <div class="container-fluid activitycontainer">
    <div class="row arow">
     <div class="col-md-6 offset-md-4">
@@ -78,10 +10,10 @@
 		 <a href="#contact"><button onclick="myFunction3()" class="btn btn-dark" id="acbutton">SHOPPING</button></a>
 	    </div>
       </div>
-  </div>	
+  </div>
 	 <br>
 	                                  <!-- Sight seeing-->
-	 <div id="myDIV">								  
+	 <div id="myDIV">
     <div class="row arow">
 	   <div class="col-md-4">
 	     <img src="images/travel1.jpg" alt="Dudhsagar Waterfall" class="img-fluid img-thumbnail" width="70%">
@@ -102,13 +34,13 @@
 		    <p title="Basilica of Bom Jesus" class="dis">The Basilica of Bom Jesus or Borea Jezuchi Bajilika is located in Goa, India. The basilica holds the mortal remains of St. Francis Xavier. </p>
 		    <a title="Basilica of Bom Jesus" href="https://en.wikipedia.org/wiki/Basilica_of_Bom_Jesus" class="an">Read more</a>
 		</div>
-		 
+
   </div>
   </div>
                                  <!-- Activities to do-->
- <div id="myDIV2">							 
+ <div id="myDIV2">
   <div class="row arow">
-     <div class="col-md-4">							 
+     <div class="col-md-4">
         <img src="images/activity1.jpg" alt="water sports" class="img-fluid img-thumbnail" width="70%">
 		  <h1>Adventure water sports</h1>
 		  <p title="Adventure water sports">A trip to Goa can never be complete with enjoying water sports!One of the best hubs for water sports in the country,the beaches and the waterfronts of Goa make it<br> for an international water sports destination.</p>
@@ -128,32 +60,25 @@
 		  <a title="Hiking" href="https://www.thrillophilia.com/tours/jungle-adventure-in-goa" class="an">Book now</a>
 		</div>
     </div>
- </div>	
+ </div>
 		                              <!-- Shopping-->
-		<div id="myDIV3">							  
+		<div id="myDIV3">
 		 <div class="row arow">
            <div class="col-md-4">
 		     <img src="images/market1.jpeg" alt="Anjuna Flea Market" class="img-fluid img-thumbnail" width="70%">
 		     <h1>Anjuna Flea Market</h1>
 		     <p title="Anjuna Flea Market">The liveliest of all markets in Goa,it is held every Wednesday in the beach town of Anjuna in North<br> Goa. </p>
-			</div> 
+			</div>
 			<div class="col-md-4">
 			   <img src="images/market2.jpg"  alt="Calangute Market Square" class="img-fluid img-thumbnail" width="70%">
 		       <h1>Calangute Market Square</h1>
 		       <p title="Calangute Market Square">The popular Calangute beach in Goa is lined with innumerable shacks and stalls making it one of the top markets of this shopping paradise.</p>
-			 </div> 
+			 </div>
 			 <div class="col-md-4">
 			    <img src="images/market3.jpg" alt="Saturday Night Bazaar at Arpora" class="img-fluid img-thumbnail" width="70%" >
 		        <h1>Saturday Night Bazaar at Arpora</h1>
 		        <p title="Saturday Night Bazaar at Arpora">There is no better way to spend a Saturday evening in Goathan by visiting the Night Bazaar at Arpora.</p>
-			 </div> 
+			 </div>
 		</div>
 	</div>
   </div>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
-</html>
--- footer.php
+++ footer.php
@@ -1,21 +1,4 @@
 
-                                <!-- Footer Section on Home page-->
-<!DOCTYPE html>
-<html lang="en-US">
-
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Rajmahal hotel"> 
-    <meta name="keywords" content="Rajmahal hotel, hotel in Goa, best hotel"> 
-    <meta name="author" content="Sonal Sharma"> 
-    <meta charset="UTF-8">
-	 <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/footercss.css">
-  </head>
-  <body>
     <footer>
 	  <div class="container-fluid footbox">
 	    <div class="row">
@@ -30,7 +13,7 @@
 		    <b>Web</b>:<span class="tab"><a href="#gg" class="alink">www.rajmahal-hotel.com </a></span>
 		    </address>
 	      </div>
-	   
+
 	     <div class="col-sm-5 offset-sm-2">
 	      <font size="6" color="white"><i> ABOUT US</i></font><br>
 		  <p title="about us">Strategically located, Hotel Rajmahal stands one of the tallest hotel in Goa overlooking the Garden City so for as that you can see. Its history of over five decades has a perfect platform to understand every guest to emphasis on the delicate care. The 260-room hotel has carved a niche for itself from business class to executive class travellers.Yes you are right it's "the most preferred business class hotel". </p>
@@ -38,6 +21,3 @@
 	  </div>
 	</div>
 	</footer>
-  </body>
-  
-</html>
